12427605c59ec8ff702e209b284707049b3bb4b1
Mckeown-Parrot <jameskwaku2020@outlook.com>
2023-08-31 11:23:26 +0000

    apps update

--- apps.py
+++ apps.py
@@ -4,13 +4,13 @@ from flask import render_template
 
 app = Flask(__name__, )
 
-@app.route('/')
+@app.route('/mail')
 def index():
-    return render_templates('index.html')
+    return render_template('index.html')
 
 @app.route('/whoami')
 def whoami():
     return 'I am James McKeown'
-    
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8181)
